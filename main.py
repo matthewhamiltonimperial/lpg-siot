9197a0fea7fc30c0c86c3a94f04a564922465cd1
matthewhamiltonimperial <32711242+matthewhamiltonimperial@users.noreply.github.com>
2019-11-28 17:13:32 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,46 @@ import time
 import bluetooth
 import sys
 import subprocess
+import os
+import glob
+from ISStreamer.Streamer import Streamer
+
+streamer = Streamer(bucket_name="Weight and Temperature Stream", bucket_key="piot_temp_stream031815", access_key="ist_7wHIHYwnwlSlqFtbB_T3akOmk4yld5fr")
+
+
+#FOR DSB18B20 Temperature Sensor
+
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        return temp_c
+
+while True:
+    temp_c = read_temp()
+    streamer.log("temperature (C)", temp_c)
+    print str(temp_c) + " C"
+    time.sleep(.5)
+
+#FOR Wii Balance Board (scales)
 
 # --------- User Settings ---------
 WEIGHT_SAMPLES = 250
@@ -49,6 +89,7 @@ class EventProcessor:
                 self._weight = self._sum/WEIGHT_SAMPLES
                 self._measureCnt = 0
                 print str(self._weight) + " Kg"
+                streamer.log("weight (Kg)", self._weight)
             if not self._measured:
                 self._measured = True
 
